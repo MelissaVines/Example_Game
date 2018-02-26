93344552d014d8ebfc60a4738cdc6452252cd05b
MelissaVines <melissamvines@sky.com>
2018-02-26 11:40:16 +0000

    Example game code

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public string logText = "Hello world again";
+	public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(logText);
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+		rigidBody.velocity = Vector2.right * speed;
+	}
+}
