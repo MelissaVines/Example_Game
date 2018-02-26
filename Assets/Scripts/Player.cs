89b1b5047100375d734656d7f9ebbf8843487b2b
MelissaVines <melissamvines@sky.com>
2018-02-26 12:47:51 +0000

    More example game code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,20 @@ public class Player : MonoBehaviour {
 
 	public string logText = "Hello world again";
 	public float speed = 2;
+	public float health = 10;
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log(logText);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(logText);
 		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
 		rigidBody.velocity = Vector2.right * speed;
+	}
+
+	public void ApplyDamage (float damageDealt) {
+		health = health - damageDealt;
 	}
 }
