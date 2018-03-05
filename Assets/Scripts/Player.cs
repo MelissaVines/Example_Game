48e9403ea275e09bc5ac1006207db0b8520ce653
MelissaVines <melissamvines@sky.com>
2018-03-05 11:07:06 +0000

    Added player controls for moving and jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour {
 
 	public string logText = "Hello world again";
 	public float speed = 2;
+	public float jumpSpeed = 50;
 	public float health = 10;
 
 	// Use this for initialization
@@ -14,8 +15,36 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// Getting the rigidbody from the game object we are attached to
 		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
-		rigidBody.velocity = Vector2.right * speed;
+		// Number between -1 and 1 based on player pressing left or right
+		float horizontal = Input.GetAxis("Horizontal");
+		// Boolean (true or false) based on player pressing spacebar
+		bool jump = Input.GetButtonDown("Jump");
+		// Find out if we are touching the ground
+		// Get the collider compontent attatched to this object
+		Collider2D collider = GetComponent<Collider2D>();
+		// Find out if we are colliding with the ground
+		LayerMask groundLayer = LayerMask.GetMask("Ground");
+		bool touchingGround = collider.IsTouchingLayers(groundLayer);
+		//Debug.Log(horizontal);
+		// Cache a local copy of our rigidbody's velocity
+		Vector2 velocity = rigidBody.velocity;
+		// Set the x (left/right) component of the velocity based on our input
+		velocity.x = horizontal * speed;
+		// Set the y (up/dowm) component of the velocity based on jump
+		if (jump == true && touchingGround == true) {
+			velocity.y = jumpSpeed;
+		}
+		// Set our rigidbody's velocity based on our local copy
+		rigidBody.velocity = velocity;
+		// Print a log when the mouse button is pressed
+		if (Input.GetMouseButton (0)) {
+			Debug.Log ("Mouse button down");
+		}
+		// Print a log of the mouse position
+		Vector2 mousePosition = Input.mousePosition;
+		Debug.Log ("Mouse position is " + mousePosition);
 	}
 
 	public void ApplyDamage (float damageDealt) {
